62b64e1b6299080ba7eaaa8211ee24751021d032
Ankush Kumar <ankushkashyap0000@gmail.com>
2021-08-07 15:05:41 +0530

    added subCategory option in addProduct and populate based on the category choosen

--- src/components/pages/vendor/Product/CSS/AddProductStyles.js
+++ src/components/pages/vendor/Product/CSS/AddProductStyles.js
@@ -1,9 +1,12 @@
 import { makeStyles } from "../../../../Design/MyUseStyles";
+
 const AddProductStyles = makeStyles({
+  // Category and subcategory styles are same
   CategorySelect: {
     width: "100%",
     padding: "12px 10px",
     marginBottom: "10px",
+    borderRadius: "5px",
   },
   categoryLabel: {
     marginLeft: "5px",
